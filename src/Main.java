65249b6095276cc7b1d8ce726dfd29d666dbbf25
621352 <camarenburt>
2023-10-11 09:05:03 -0400

    erwre

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int month;
+
+        System.out.println("Please input your birth month by number.");
+        month = scan.nextInt();
+
+        if (month >=1 && month <= 12){
+            System.out.println("Your birth birth month is " + month + ".");
+        }else{
+            System.out.println("Your input is invalid please try again.");
+
+        }
     }
 }
